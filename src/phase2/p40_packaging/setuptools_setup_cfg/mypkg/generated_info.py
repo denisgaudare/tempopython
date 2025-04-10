db60f81c43e2c60bd645e1a25f3e1cecccde253a
Denis Gaudare <denisgaudare@gmail.com>
2025-04-10 17:08:01 +0200

    denisgaudare@gmail.com

---
+++ src/phase2/p40_packaging/setuptools_setup_cfg/mypkg/generated_info.py
@@ -0,0 +1,2 @@
+# Ce fichier a été généré automatiquement
+BUILD_DATE = 'XYZ'
--- src/phase2/p40_packaging/setuptools_setup_cfg/setup.py
+++ src/phase2/p40_packaging/setuptools_setup_cfg/setup.py
@@ -1,8 +1,27 @@
+import datetime
+import re
+from pathlib import Path
+
 from setuptools import setup, find_packages
+
+def get_version():
+    content = Path("mypkg/__init__.py").read_text()
+    match = re.search(r'__version__\s*=\s*"(.+?)"', content)
+    return match.group(1)
+
+# Exemple de génération de fichier dynamique
+with open("mypkg/generated_info.py", "w") as f:
+    f.write(f'# Ce fichier a été généré automatiquement\n')
+    f.write(f"BUILD_DATE = 'XYZ'")
+
 
 setup(
     name="mypkg",
-    version="0.1.0",
-    packages=find_packages(),
+    install_requires=[
+        'requests>=2.0',
+        'numpy~=1.24.0',
+    ],
+    version=get_version(),
+    packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": ["mypkg=mypkg.cli:main"]},
 )
